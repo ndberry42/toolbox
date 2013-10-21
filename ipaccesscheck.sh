41cab01d103b5663d67f622043c113304b8b2954
ndberry42 <nolan.berry@rackspace.com>
2013-10-21 13:28:47 -0500

    added whois support for this

--- ipaccesscheck.sh
+++ ipaccesscheck.sh
@@ -30,13 +30,13 @@ echo "
 ##########################
 
 echo "Checking webserver type(Only Supports httpd and apache2 at the moment)"
-	if [ -d /etc/httpd/ ]; then
-		WEBSERVER=httpd
-		echo "Server running httpd";
-	else
-		WEBSERVER=apache2
-		echo "Server running apache2";
-	fi
+        if [ -d /etc/httpd/ ]; then
+                WEBSERVER=httpd
+                echo "Server running httpd";
+        else
+                WEBSERVER=apache2
+                echo "Server running apache2";
+        fi
 
 ##########################
 #Check and Parse the logs#
@@ -45,8 +45,8 @@ DATE1="$(date|awk '{print $3}')"
 DATE2="$(date|awk '{print $2}')"
 TODAY="$DATE1"/"$DATE2"
 echo "Checking access logs for most frequent IPs from today:"
-cat /var/log/$WEBSERVER/*access*|grep $TODAY|awk '{print $1}'|sort -n|uniq -c|sort -n|head -n 25
-
-
-
-
+cat /var/log/$WEBSERVER/*access*|grep $TODAY|awk '{print $1}'|sort -n|uniq -c|sort -n|tail -n 20
+echo "Getting Country Information for IPs"
+cat /var/log/$WEBSERVER/*access*|grep $TODAY|awk '{print $1}'|sort|uniq|while IFS= read -r ip ; do
+    whois "$ip"|grep 'Country'
+done
