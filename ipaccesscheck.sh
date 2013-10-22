03e7581452bacfc4f43c3b7b99fcb717a06c6ef7
ndberry42 <nolan.berry@rackspace.com>
2013-10-21 21:21:30 -0500

    fixed a few things

--- ipaccesscheck.sh
+++ ipaccesscheck.sh
@@ -41,12 +41,20 @@ echo "Checking webserver type(Only Supports httpd and apache2 at the moment)"
 ##########################
 #Check and Parse the logs#
 ##########################
-DATE1="$(date|awk '{print $3}')"
-DATE2="$(date|awk '{print $2}')"
-TODAY="$DATE1"/"$DATE2"
-echo "Checking access logs for most frequent IPs from today:"
-cat /var/log/$WEBSERVER/*access*|grep $TODAY|awk '{print $1}'|sort -n|uniq -c|sort -n|tail -n 20
-echo "Getting Country Information for IPs"
-cat /var/log/$WEBSERVER/*access*|grep $TODAY|awk '{print $1}'|sort|uniq|while IFS= read -r ip ; do
-   echo $ip:$(whois "$ip"|grep 'Country'|awk '{print $2}')
-done
+
+if [ -s /var/log/$WEBSERVER/ ]; then
+
+	DATE1="$(date|awk '{print $3}')"
+	DATE2="$(date|awk '{print $2}')"
+	TODAY="$DATE1"/"$DATE2"
+
+	echo "Checking access logs for most frequent IPs from today:"
+	cat /var/log/$WEBSERVER/*access*|grep $TODAY|awk '{print $1}'|sort -n|uniq -c|sort -n|tail -n 20
+	echo "Getting Country Information for IPs"
+	cat /var/log/$WEBSERVER/*access*|grep $TODAY|awk '{print $1}'|sort|uniq|while IFS= read -r ip ; do
+   	echo $ip:$(whois "$ip"|grep 'Country'|awk '{print $2}')
+	done
+else
+	echo "No webserver found.";
+fi
+
